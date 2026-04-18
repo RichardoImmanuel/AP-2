4acaa1f3c58a9a607dc9ddce73053e7dac38ec8e
RichardoImmanuel <humikarichardo@students.usu.ac.id>
2026-04-18 09:46:04 +0700

    Pertemuan 5 : Array & Vector

---
+++ Pertemuan 5/vector.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    // Vector declaration & Initialization
+    vector<string> nama_karyawan = {"Alya", "Parul", "Hana", "Dewi", "Padhil"};
+
+    // // Menampilkan data ascending
+    // for (int i = 0; i < nama_karyawan.size(); i++){
+    //     cout << nama_karyawan[i] << endl;
+    // }
+
+    // Add data to vector
+    nama_karyawan.push_back("Edric");
+
+    // Menampilkan data ascending
+    // for (int i = 0; i < nama_karyawan.size(); i++){
+    //     cout << nama_karyawan[i] << endl;
+    // }
+
+    // Delete data from vector
+    nama_karyawan.pop_back();
+    nama_karyawan.erase(nama_karyawan.begin() +2);
+
+    for (int i = 0; i < nama_karyawan.size(); i++){
+        cout << nama_karyawan[i] << endl;
+    }
+
+    
+}
